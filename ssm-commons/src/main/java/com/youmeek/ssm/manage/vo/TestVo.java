1563c6c410518684564fca3062c447e406e7cceb
pcoding <1557100211@qq.com>
2017-04-19 10:47:19 +0800

    test commit

--- ssm-commons/src/main/java/com/youmeek/ssm/manage/vo/TestVo.java
+++ ssm-commons/src/main/java/com/youmeek/ssm/manage/vo/TestVo.java
@@ -4,4 +4,7 @@ package com.youmeek.ssm.manage.vo;
  * Created by dinozoo on 2017/4/19 0019.
  */
 public class TestVo {
+    public static void main(String[] args) {
+        System.out.printf("123");
+    }
 }
